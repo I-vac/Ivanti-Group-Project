0b00ff0785a033711f984f72ce45f0fecb7c3200
a.joshi@student.fontys.nl <m.j.arpit235@gmail.com>
2021-11-04 12:37:27 +0100

    Fucking Shit commited !

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -10,15 +10,6 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    jQuery(document).ready(function($){
-      $(".dropdown-button").dropdown();
-      $('.modal').modal();
-      $(".signup-toggle").click(function(){
-        $(this).hide();
-        $(".signupForm").show(300);
-        $(".policy").css("visibility","visible");
-      });
-    });
 
 
   }
