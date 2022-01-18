371041e2078dd83d181828890105a4eadcb01203
Alharthy,Ahmed A.S.A <425113@student.fontys.nl>
2022-01-18 13:58:13 +0100

    functional package creation (Not user friendly yet)

--- client/src/app/register-component/register-component.component.ts
+++ client/src/app/register-component/register-component.component.ts
@@ -26,15 +26,44 @@ export class RegisterComponentComponent implements OnInit {
     this.downloadFile = <HTMLElement>document.querySelector("#downloadFileUrl");
   }
 
+  createEventListener() {
+    console.log("worked");
+    document.getElementById("uploadButton").addEventListener('click', (function () {
+      console.log("worked")
+
+      const files = this.uploadFormInput.files;
+      console.log("work");
+      if (files.length !== 0) {
+        this.uploadFile(files[0]);
+        event.preventDefault();
+      } else {
+        alert('Please select a file')
+      }
+
+    }).bind(this), true);
+
+  }
+
+  uploadFile(file) {
+    let formData = new FormData();
+    formData.append("file", file);
+
+    let req = new XMLHttpRequest();
+    req.open("POST", "http://localhost:8080/file/upload")
+
+    req.onload = (function () {
+      this.fileid = req.responseText;      
+    }).bind(this)
+
+    req.send(formData);
+
+  }
+
   onSubmit(): void {
     var title = ((document.getElementById("title") as HTMLInputElement).value);
     var categories = ((document.getElementById("categories") as HTMLInputElement).value);
-    var files = ((document.getElementById("fileUploadInput") as HTMLInputElement).files);
-    
-      this.httpService.createFile(files[0])
-
-      console.log("ollaa")
-
+    var fileid = null;
+    console.log(this.fileid);
       this.httpService.getUser()
       .subscribe(user => {
   
@@ -50,11 +79,12 @@ export class RegisterComponentComponent implements OnInit {
           } ,
           "fileId": this.fileid,
           "description": categories,
-          "title": title};
+          "title": title,
+          "installDesc" : "installDesc",
+          "os" : "os"
+        };
         this.httpService.createPackage(this.packages);
       });
-
-    
   }
 
 
